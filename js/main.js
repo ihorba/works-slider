2bcf889adc58c3015dd0c92242f0e6ed8a447f45
ihorba <ihor@footballaddicts.com>
2017-12-19 19:04:03 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($){
+	var teamSlider = $('.ba-slider');
+	teamSlider.slick({
+		slide: '.ba-slide',
+		arrows: true,
+		prevArrow: '.ba-slider-prev',
+		nextArrow: '.ba-slider-next',
+		slidesToShow: 4,
+		slidesToScroll:1,
+	});
+
+})(jQuery);
